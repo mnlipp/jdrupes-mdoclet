7c2ed3d8d51c0d92d488faf06b1f3c53e33ee53c
Michael N. Lipp <mnl@mnl.de>
2021-03-23 21:20:53 +0100

    Add special test files.

---
+++ mdoclet/testfiles/overview.md
@@ -0,0 +1,4 @@
+The default behavior is equivalent to "``--markdown-processor 
+{@link org.jdrupes.mdoclet.processors.FlexmarkProcessor 
+org.jdrupes.mdoclet.processors.FlexmarkProcessor}``".
+ 
---
+++ mdoclet/testfiles/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+/**
+ * Simple test file.
+ *
+ * Smaller (<) and greater (>).
+ *
+ * Smaller (`<`) and greater (`>`) as code
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
