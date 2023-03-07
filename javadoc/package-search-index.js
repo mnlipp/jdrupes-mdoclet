ef57815ef011f9def3900fe85b88b4a9151d218e
Michael N. Lipp <mnl@mnl.de>
2023-03-07 14:15:03 +0100

    Update.

--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.jdrupes.mdoclet"},{"l":"org.jdrupes.mdoclet.processors"},{"l":"org.jdrupes.mdoclet.processors.flexmark"}];updateSearchResults();
